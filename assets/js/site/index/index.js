0f28e9109328175a8f191bf9830fb84b3bb8a68c
AlexSoul1903 <alexmanuel19frias@gmail.com>
2022-10-09 00:15:15 -0400

    Proyecto actualizado

--- assets/js/site/index/index.js
+++ assets/js/site/index/index.js
@@ -6,8 +6,7 @@ const input_correo = document.getElementById("correo");
 const input_telefono = document.getElementById("telefono");
 const input_pag_web = document.getElementById("pagina");
 
-const input_idiomas = document.getElementById("idiomas");
-
+let input_idiomas = document.getElementById("idiomas");
 const input_empleos = document.getElementById("empleos");
 const input_educacion = document.getElementById("educacion");
 
@@ -17,9 +16,251 @@ const input_resumen = document.getElementById("resumen");
 
 function Crear_CV() {
 
+
+    const nombreContacto = input_nombre.value;
+    const nombreDireccion = input_direccion.value;
+
+    const nombreTelefono = input_telefono.value;
+    const nombrePagina = input_pag_web.value;
+
+    const nombreIdiomas = input_idiomas.value;
+
+    const nombreCorreo = input_correo.value;
+
+    const nombreEmpleos = input_empleos.value;
+    const nombreEducacion = input_educacion.value;
+
+    const nombreSkill = input_skill.value;
+
+    const nombreCertificacion = input_certificaciones.value;
+    const resumen = input_resumen.value;
+
+    const cvContainer = document.getElementById("cv-container");
+
+
     if (validar()) {
 
-        alert("Todo nitido");
+        const divrow = document.createElement("div");
+        divrow.setAttribute("class", "col-12 col-sm-12 col-md-5 col-lg-6 mt-5 offset-md-3");
+
+        const divPrincipal = document.createElement("div");
+        divPrincipal.setAttribute("class", "card");
+
+        const divHeader = document.createElement("div");
+        divHeader.setAttribute("class", "card-header bg-black text-bg-primary");
+
+        const h5TextHeader = document.createElement("h5");
+        h5TextHeader.setAttribute("class", "text-left text-dark fw-bold");
+        h5TextHeader.innerText = " ";
+
+        const divRow2 = document.createElement("div");
+        divRow2.setAttribute("class", "row");
+
+        const divCardBody = document.createElement("div");
+        divCardBody.setAttribute("class", "card-body bg-primary col-6");
+
+        const divCard2 = document.createElement("div");
+        divCard2.setAttribute("class", "card");
+
+        const h5nomText = document.createElement("h5");
+        h5nomText.setAttribute("class", "text-left text-dark fw-bold");
+        h5nomText.innerText = '' + nombreContacto;
+
+        const h5ContacText = document.createElement("h5");
+        h5ContacText.setAttribute("class", "text-left text-primary fw-semibold");
+        h5ContacText.innerText = "Contacto.";
+
+        const ulDatosCont = document.createElement("ul");
+        ulDatosCont.setAttribute("class", "list-group list-group-flush");
+
+        const liDireccion = document.createElement("li");
+        liDireccion.setAttribute("class", "list-group-item");
+        liDireccion.innerText = "Direccion: " + nombreDireccion;
+
+        const liTelefono = document.createElement("li");
+        liTelefono.setAttribute("class", "list-group-item");
+        liTelefono.innerText = "Telefono: " + nombreTelefono;
+
+        const liCorreo = document.createElement("li");
+        liCorreo.setAttribute("class", "list-group-item");
+        liCorreo.innerText = "Correo: " + nombreCorreo;
+
+        const liPagina = document.createElement("li");
+        liPagina.setAttribute("class", "list-group-item");
+        liPagina.innerText = "Pagina: " + nombrePagina;
+
+
+        const h5Idiomas = document.createElement("h5");
+        h5Idiomas.setAttribute("class", "text-left fw-semibold fw-semibold text-primary mt-4");
+        h5Idiomas.innerText = "Idiomas.";
+
+        const ulDatosIdioma = document.createElement("ul");
+        ulDatosIdioma.setAttribute("class", "list-group list-group-flush");
+
+        const divCardBody2 = document.createElement("div");
+        divCardBody2.setAttribute("class", "card-body bg-secondary col-6");
+
+        const divCard3 = document.createElement("div");
+        divCard3.setAttribute("class", "card");
+
+        const h5Resumen2 = document.createElement("h5");
+        h5Resumen2.setAttribute("class", "text-left fw-semibold fw-semibold text-primary mt-4");
+        h5Resumen2.innerText = "Resumen.";
+
+        const h5ResumenEscri = document.createElement("h5");
+        h5ResumenEscri.setAttribute("class", "text-left text-bg-light fw-light");
+        h5ResumenEscri.innerText = "" + resumen;
+
+        const h5Skill = document.createElement("h5");
+        h5Skill.setAttribute("class", "text-left fw-semibold fw-semibold text-primary mt-4");
+        h5Skill.innerText = "Skills.";
+
+        const ulSkills = document.createElement("ul");
+        ulSkills.setAttribute("class", "list-group list-group-flush");
+
+
+        const h5Experiencia = document.createElement("h5");
+        h5Experiencia.setAttribute("class", "text-left fw-semibold fw-semibold text-primary mt-4");
+        h5Experiencia.innerText = "Experiencia.";
+
+        const ulExperiencia = document.createElement("ul");
+        ulExperiencia.setAttribute("class", "list-group list-group-flush");
+
+        const h5Educacion = document.createElement("h5");
+        h5Educacion.setAttribute("class", "text-left fw-semibold fw-semibold text-primary mt-4");
+        h5Educacion.innerText = "Educación.";
+
+        const ulEducacion = document.createElement("ul");
+        ulEducacion.setAttribute("class", "list-group list-group-flush");
+
+        const h5Certificaciones = document.createElement("h5");
+        h5Certificaciones.setAttribute("class", "text-left fw-semibold fw-semibold text-primary mt-4");
+        h5Certificaciones.innerText = "Certificaciones";
+
+        const ulCertificaciones = document.createElement("ul");
+        ulCertificaciones.setAttribute("class", "list-group list-group-flush");
+
+        const eliminarBTN = document.createElement("button");
+        eliminarBTN.setAttribute("class", "btn btn-danger float-end mt-2 mx-2");
+        eliminarBTN.innerText = "Eliminar";
+        eliminarBTN.addEventListener("click", function() {
+
+            if (confirm("Estas seguros de que quieres borrar el CV?")) {
+
+                cvContainer.removeChild(divrow);
+
+
+            }
+
+        });
+
+        cvContainer.appendChild(divrow);
+        divrow.appendChild(divPrincipal);
+        divPrincipal.appendChild(divHeader);
+        divHeader.appendChild(h5TextHeader);
+        divPrincipal.appendChild(divRow2);
+        divRow2.appendChild(divCardBody);
+        divRow2.appendChild(divCardBody2);
+        divCardBody.appendChild(divCard2);
+        divCard2.appendChild(h5nomText);
+        divCard2.appendChild(h5ContacText);
+        divCard2.appendChild(ulDatosCont);
+        ulDatosCont.appendChild(liDireccion);
+        ulDatosCont.appendChild(liTelefono);
+        ulDatosCont.appendChild(liCorreo);
+        ulDatosCont.appendChild(liPagina);
+        divCard2.appendChild(h5Idiomas);
+        divCard2.appendChild(ulDatosIdioma);
+
+        const Divisor_Idiomas = nombreIdiomas.split(",");
+
+        const Divisor_Skills = nombreSkill.split(",");
+
+        const Divisor_Experiencia = nombreEmpleos.split(",");
+        const Divisor_Educacion = nombreEducacion.split(",");
+
+        const Divisor_Certificaciones = nombreCertificacion.split(",");
+
+
+        for (i = 0; i < Divisor_Idiomas.length; i++) {
+
+            if (Divisor_Idiomas[i] != ",") {
+                const liIdioma = document.createElement("li");
+                liIdioma.setAttribute("class", "list-group-item");
+                liIdioma.innerText = "" + Divisor_Idiomas[i];
+                ulDatosIdioma.appendChild(liIdioma);
+            }
+        }
+        divCardBody2.appendChild(divCard3);
+        divCardBody2.appendChild(eliminarBTN);
+
+        divCard3.appendChild(h5Resumen2);
+        divCard3.appendChild(h5ResumenEscri);
+        divCard3.appendChild(h5Skill);
+        divCard3.appendChild(ulSkills);
+
+        for (i = 0; i < Divisor_Skills.length; i++) {
+
+            if (Divisor_Skills[i] != ",") {
+                const liSkill = document.createElement("li");
+                liSkill.setAttribute("class", "list-group-item");
+                liSkill.innerText = "" + Divisor_Skills[i];
+                ulSkills.appendChild(liSkill);
+            }
+        }
+
+        divCard3.appendChild(h5Experiencia);
+        divCard3.appendChild(ulExperiencia);
+
+        for (i = 0; i < Divisor_Experiencia.length; i++) {
+
+            if (Divisor_Experiencia[i] != ",") {
+                const liExperiencia = document.createElement("li");
+                liExperiencia.setAttribute("class", "list-group-item");
+                liExperiencia.innerText = "" + Divisor_Skills[i];
+                ulExperiencia.appendChild(liExperiencia);
+            }
+        }
+
+        divCard3.appendChild(h5Educacion);
+        divCard3.appendChild(ulEducacion);
+
+        for (i = 0; i < Divisor_Educacion.length; i++) {
+
+            if (Divisor_Educacion[i] != ",") {
+                const liEducacion = document.createElement("li");
+                liEducacion.setAttribute("class", "list-group-item");
+                liEducacion.innerText = "" + Divisor_Educacion[i];
+                ulEducacion.appendChild(liEducacion);
+            }
+        }
+
+
+
+        divCard3.appendChild(h5Certificaciones);
+
+        divCard3.appendChild(ulCertificaciones);
+        for (i = 0; i < Divisor_Certificaciones.length; i++) {
+
+            if (Divisor_Certificaciones[i] != ",") {
+                const liCertificaciones = document.createElement("li");
+                liCertificaciones.setAttribute("class", "list-group-item");
+                liCertificaciones.innerText = "" + Divisor_Skills[i];
+                ulCertificaciones.appendChild(liCertificaciones);
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        Cls();
     } else {
 
         alert("Debe llenar todos los campos");
